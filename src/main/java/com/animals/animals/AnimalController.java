d4f6b1e344d7ea73fe920b3f029c6fb14cd08f03
Demir Boraj <demirboraj97@gmail.com>
2022-10-06 02:59:33 +0200

    first commit

---
+++ src/main/java/com/animals/animals/AnimalController.java
@@ -0,0 +1,5 @@
+package com.animals.animals;
+
+public class AnimalController {
+    
+}
---
+++ src/test/java/com/animals/animals/AnimalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animals.animals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
